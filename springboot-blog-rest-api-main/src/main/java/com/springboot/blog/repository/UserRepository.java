fade432e72f71aec791b2d3d353a5ff2a51fc9f3
Jay <jaypatelmh2021@gmail.com>
2022-07-31 09:23:39 +0530

    Secuirty

---
+++ springboot-blog-rest-api-main/src/main/java/com/springboot/blog/repository/UserRepository.java
@@ -0,0 +1,18 @@
+package com.springboot.blog.repository;
+
+import com.springboot.blog.entity.User;
+import org.springframework.data.domain.Example;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+	@Query("from User where email=:email")
+    public Optional<User> findByEmail(@Param("email") String email);
+    Optional<User> findByUsernameOrEmail(String username, String email);
+    Optional<User> findByUsername(String username);
+    Boolean existsByUsername(String username);
+    Boolean existsByEmail(String email);
+}
